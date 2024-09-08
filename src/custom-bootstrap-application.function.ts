10f4c9a002d9a5b3103831321f6fc69f54725aeb
Sergei Klimov <sergei.klimov@b2broker.com>
2024-09-08 21:22:53 +0400

    feat: add comments, refactoring
    feature-user-list

--- src/custom-bootstrap-application.function.ts
+++ src/custom-bootstrap-application.function.ts
@@ -3,11 +3,14 @@ import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MockBackendInterceptor } from './app/shared/mock-backend/mock-backend.interceptor';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app/app-routes.const';
 
+// Used to remove the App Module and migrate to standalone App Component.
 export const customBootstrapApplication = async (): Promise<ApplicationRef> => {
-
   return bootstrapApplication(AppComponent, {
     providers: [
+      provideRouter(APP_ROUTES),
       importProvidersFrom(BrowserModule, HttpClientModule),
       {
         provide: HTTP_INTERCEPTORS,
